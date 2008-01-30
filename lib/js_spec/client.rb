a57fc14572d4305f9ce48974aa5de6b06862b853
btakita <btakita@af276e61-6b34-4dac-905b-574b5f35ef33>
2008-01-30 01:48:57 +0000

    CTI/BT - Moved DEFAULT_HOST and DEFAULT_PORT into JsSpec
    git-svn-id: svn+ssh://rubyforge.org/var/svn/pivotalrb/js_spec/trunk@1231 af276e61-6b34-4dac-905b-574b5f35ef33

--- lib/js_spec/client.rb
+++ lib/js_spec/client.rb
@@ -3,7 +3,7 @@ module JsSpec
   class Client
     class << self
       def run
-        response = Net::HTTP.start(JsSpec::DEFAULT_HOST, JsSpec::DEFAULT_PORT) do |http|
+        response = Net::HTTP.start(DEFAULT_HOST, DEFAULT_PORT) do |http|
           http.post('/runners/firefox', {})
         end
 
