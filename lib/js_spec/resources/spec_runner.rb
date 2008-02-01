5bd20046e3a0781339dcc7561eae3b06fd17e2e2
btakita <btakita@af276e61-6b34-4dac-905b-574b5f35ef33>
2008-02-01 21:50:14 +0000

    Reformatting.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/pivotalrb/js_spec/trunk@1254 af276e61-6b34-4dac-905b-574b5f35ef33

--- lib/js_spec/resources/spec_runner.rb
+++ lib/js_spec/resources/spec_runner.rb
@@ -1,8 +1,8 @@
 module JsSpec
   module Resources
     class SpecRunner
-    def get
-      html = <<-HTML
+      def get
+        html = <<-HTML
         <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
         <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ko">
         <head>
@@ -13,18 +13,18 @@ module JsSpec
         <script type="text/javascript" src="/core/JSSpec.js"></script>
         <script type="text/javascript" src="/core/jquery.js"></script>
         <script type="text/javascript" src="/core/JSSpecExtensions.js"></script>
-      HTML
-      spec_files.each do |file|
-        html << %{<script type="text/javascript" src="#{file.relative_path}"></script>\n}
-      end
+        HTML
+        spec_files.each do |file|
+          html << %{<script type="text/javascript" src="#{file.relative_path}"></script>\n}
+        end
 
-      html << <<-HTML
+        html << <<-HTML
         </head>
         <body></body>
         </html>
-      HTML
-      html.gsub(/^        /, "")
+        HTML
+        html.gsub(/^        /, "")
+      end
     end
-  end
   end
 end
