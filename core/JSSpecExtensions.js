21bfd8655e231ebc840e33b096257e5723f61779
btakita <btakita@af276e61-6b34-4dac-905b-574b5f35ef33>
2008-03-21 01:13:10 +0000

    CTI/BT - Added Spec and require onload handler.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/pivotalrb/js_spec/trunk@1336 af276e61-6b34-4dac-905b-574b5f35ef33

--- core/JSSpecExtensions.js
+++ core/JSSpecExtensions.js
@@ -1,20 +1,67 @@
-var Require = {
-  require: function(path_from_javascripts) {
-    if(!Require.required_paths[path_from_javascripts]) {
+function Spec() {
+}
+
+Spec.register = function(spec_constructor) {
+  spec_constructor['before each'] = function() {};
+  spec_constructor['after each'] = function() {
+    reset_spec_environment();
+  }
+
+  spec_constructor.describe = function(context, definition) {
+    var custom_before = definition['before each'];
+    if(custom_before) {
+      definition['before each'] = function() {
+        spec_constructor['before each']();
+        custom_before();
+      }
+    }
+
+    var custom_after = definition['after each'];
+    if(custom_after) {
+      definition['after each'] = function() {
+        spec_constructor['after each']();
+        custom_after();
+      }
+    }
+    new spec_constructor();
+    describe(this.constructor.name + context, definition);
+  }
+}
+
+var Assets = {
+  require: function(path_from_javascripts, onload) {
+    if(!Assets.required_paths[path_from_javascripts]) {
       var full_path = path_from_javascripts + ".js";
-      if (Require.use_cache_buster) {
-        full_path += '?' + Require.cache_buster;
+      if (Assets.use_cache_buster) {
+        full_path += '?' + Assets.cache_buster;
       }
       document.write("<script src='" + full_path + "' type='text/javascript'></script>");
-      Require.required_paths[path_from_javascripts] = true;
+      if(onload) {
+        var scripts = document.getElementsByTagName('script');
+        scripts[scripts.length-1].onload = onload;
+      }
+      Assets.required_paths[path_from_javascripts] = true;
+    }
+  },
+
+  stylesheet: function(path_from_stylesheets) {
+    if(!Assets.included_stylesheets[path_from_stylesheets]) {
+      var full_path = path_from_stylesheets + ".css";
+      if(Assets.use_cache_buster) {
+        full_path += '?' + Assets.cache_buster;
+      }
+      document.write("<link rel='stylesheet' type='text/css' href='" + full_path + "' />");
+      Assets.included_stylesheets[path_from_stylesheets] = true;
     }
   },
 
   required_paths: {},
-  use_cache_buster: true, // TODO: NS/CTI - make this configurable from the UI.
+  included_stylesheets: {},
+  use_cache_buster: false, // TODO: NS/CTI - make this configurable from the UI.
   cache_buster: parseInt(new Date().getTime()/(1*1000))
 }
-var require = Require.require;
+var require = Assets.require;
+var stylesheet = Assets.stylesheet;
 
 var URL_REGEX = /^((\w+):\/\/)(([^:]+):?([^@]+)?@)?([^\/\?:]*):?(\d+)?(\/?[^\?#]+)?\??([^#]+)?#?(.+)?/;
 function parse_url(url) {
@@ -146,7 +193,3 @@ JSSpec.DSL.Subject.prototype.should_not_raise = function(expected) {
     throw JSSpec._assertionFailure;
   }
 }
-
-
-
-
